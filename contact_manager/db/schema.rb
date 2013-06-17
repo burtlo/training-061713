ce6214e861ca311c3cb133459c3332028be75890
Franklin Webber <franklin.webber@gmail.com>
2013-06-17 11:30:08 -0700

    New and improved migration and schema for people

--- contact_manager/db/schema.rb
+++ contact_manager/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130617181622) do
+ActiveRecord::Schema.define(:version => 20130617182909) do
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
