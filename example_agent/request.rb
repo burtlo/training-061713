5cbd930ee3fc26b2d5efbdcd7fea373a3e6c360c
Franklin Webber <franklin.webber@gmail.com>
2013-06-19 14:53:23 -0700

    Updating to use hashie

--- example_agent/request.rb
+++ example_agent/request.rb
@@ -13,4 +13,5 @@ first_person =  people.first
 
 puts first_person.class
 puts first_person.name
-puts first_person.phone_numbers.first
+puts first_person.phone_numbers.first.number
+puts first_person.primary_phone_number
